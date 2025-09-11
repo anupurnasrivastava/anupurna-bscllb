6518a864f3b5c2515a907c9cddbb70a6b4c3ecc0
Anupurna Srivastava <anupurnasrivastava713@gmail.com>
2025-09-11 12:07:48 +0530

    Add files via upload

---
+++ conversion by anupurna.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float f,c;
+    printf("enter temperature in fahrenhite");
+    scanf("%f",&f);
+    c=((f-32)*5)/9;
+    printf("Temperature in celsius:%f",c);
+
+
+}
