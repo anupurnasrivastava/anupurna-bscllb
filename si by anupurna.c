b7dfdf231181eea8fbd515095628d7098d9a3870
Anupurna Srivastava <anupurnasrivastava713@gmail.com>
2025-09-18 12:18:40 +0530

    Add files via upload

---
+++ si by anupurna.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+float p,t,r,si;
+printf("enter the value of principle\n");
+scanf("%f",&p);
+printf("enter the value of rate of interest\n");
+scanf("%f",&r);
+printf("enter the value of time\n");
+scanf("%f",&t);
+si=(p*r*t/100);
+printf("simple interest=%f",si);
+}
