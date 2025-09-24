505ce3c1612aecf4951a0e1cf65d7be021d98c4b
Anupurna Srivastava <anupurnasrivastava713@gmail.com>
2025-09-24 15:45:18 +0530

    Add files via upload

---
+++ person is eligible to vote.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+
+int age;
+printf("enter age");
+scanf("%d",&age);
+if(age>=18)
+{
+printf("eligible for voting");
+}
+else
+{
+printf(" not eligible for voting");
+
+}
+}
